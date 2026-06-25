d893e4a0f704b7decd1147ca1bbf7a0c68583f7a
CoderAni651 <aniruddhansrikanth@gmail.com>
2026-06-25 23:10:00 +0530

    Create subbytes.v

---
+++ subbytes.v
@@ -0,0 +1,20 @@
+module subbytes(
+    input  [127:0] state_in,
+    output [127:0] state_out
+);
+
+genvar i;
+
+generate
+for(i=0;i<16;i=i+1)
+begin : AES_SBOX
+
+sbox sb(
+    .in(state_in[i*8 +: 8]),
+    .out(state_out[i*8 +: 8])
+);
+
+end
+endgenerate
+
+endmodule
